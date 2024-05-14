b9bbdc45b59dfd18450b8b60cbfbe9ac05c5cdbb
valer1yaz <valeriyazhmts@gmail.com>
2024-05-15 00:08:19 +0300

    Database and item cards

---
+++ secondhand/item/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ('name', )
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name    
+    
+class Item(models.Model):
+    Category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=False)
+    price = models.FloatField()
+    image = models.ImageField(upload_to='item_images', blank=True, null=False)
+    is_sold = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name   
